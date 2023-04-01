469a30f86a182a7c87cd93f822c19e4331e0f4c0
David Ordovskiy <david.ordovsky@gmail.com>
2023-04-01 12:45:53 -0400

    initial commit

---
+++ scanner.py
@@ -0,0 +1,33 @@
+import sys
+import socket
+from datetime import date
+
+######## UI ########
+
+def print_splash(target):
+    print("\n")
+    print("~" * 50)
+    print("Welcome to the terrible port scanner!")
+    print("(Really, this is embarrassing, just use nmap.)")
+    print("\n")
+    print("Scanning target: " + target)
+    print("~" * 50)
+
+USAGE = "Usage: python3 scanner.py <target_ip>, no CIDR"
+
+######## DO THE WORK ########
+
+# grab target IP from argv
+def define_target():
+    # this translates hostname to IPv4
+    target = socket.gethostname(sys.argv[1])
+    return target
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(USAGE)
+        sys.exit(1)
+    
+    # target = define_target()
+    print_splash(sys.argv[1])
+    
